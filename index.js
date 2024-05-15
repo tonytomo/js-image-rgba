bdf664fe593cef65d68c5fb46888439b5926e8f9
jambanhanyut <fathonisatrioutomo@student.uns.ac.id>
2024-05-15 23:56:17 +0700

    chore: Refactor CSS and improve image handling in JavaScript

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const submitButton = document.getElementById("submit-button");
 
 const resetButton = document.getElementById("reset-button");
 
+const label = document.getElementById("label");
+
 const imageCanvas = document.getElementById("image-canvas");
 
 const ctx = imageCanvas.getContext("2d");
+
+// Add function to get absolute number
 
 // If user select an image to imageFile element, create a new Image object
 imageFile.addEventListener("change", function () {
@@ -20,11 +24,31 @@ imageFile.addEventListener("change", function () {
         return;
     } else {
         message.innerHTML = "Image file loading...";
+        label.innerHTML = "";
     }
 
     reader.onload = function (e) {
         const img = new Image();
         img.onload = function () {
+            imageCanvas.width = 640;
+            imageCanvas.height = 400;
+            console.log(imageCanvas.width, imageCanvas.height);
+            if (
+                img.width > imageCanvas.width ||
+                img.height > imageCanvas.height
+            ) {
+                const scale = Math.min(
+                    imageCanvas.width / img.width,
+                    imageCanvas.height / img.height
+                );
+                imageCanvas.width = img.width * scale;
+                imageCanvas.height = img.height * scale;
+            }
+
+            console.log(imageCanvas.width, imageCanvas.height);
+
+            // imageCanvas.width = img.width;
+            // imageCanvas.height = img.height;
             ctx.drawImage(img, 0, 0, imageCanvas.width, imageCanvas.height);
             message.innerHTML = "Image file loaded!";
         };
@@ -77,6 +101,9 @@ submitButton.addEventListener("click", function () {
 // If resetButton is clicked, clear the canvas and clear the input file
 resetButton.addEventListener("click", function () {
     ctx.clearRect(0, 0, imageCanvas.width, imageCanvas.height);
+    imageCanvas.width = 640;
+    imageCanvas.height = 400;
     imageFile.value = "";
     message.innerHTML = "Image file cleared!";
+    label.innerHTML = "Drop an image here or click to upload";
 });
